0e09178df9ddfbf4ac6f3ebd44dae71ae9fdfde9
mohammad <ghmailbox72@gmail.com>
2024-11-13 02:12:32 +0330

    fix a bug in modal style

--- src/components/ModalPortal.jsx
+++ src/components/ModalPortal.jsx
@@ -6,10 +6,10 @@ function ModalPortal({ title, year, genre, director, plot, poster, onClick }) {
       onClick={onClick}
       style={{
         width: "100%",
-        height: "120vh",
-        backgroundColor: "black",
+        height: "100vh",
+        display: "flex",
         backgroundColor: "rgba(255,255,255,0.3)",
-        position: "absolute",
+        position: "fixed",
         zIndex: "9999999999999999",
       }}
     >
